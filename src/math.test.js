629ed6c9fe58da2de2e08cc86e4185d0e60b4665
fookinpelican <37412517+fookinpelican@users.noreply.github.com>
2019-10-02 23:25:09 +0200

    Update math.js tests

--- src/math.test.js
+++ src/math.test.js
@@ -1,38 +1,37 @@
 const Util = require('./math');
 
+describe('factorial', () => {
+    test('0 => 1', () => {
+        expect(Util.factorial(0)).toBe(1);
+    });
+    test('1 => 1', () => {
+        expect(Util.factorial(1)).toBe(1);
+    });
+    test('2 => 2', () => {
+        expect(Util.factorial(2)).toBe(2);
+    });
 
-test('Test factoriel de 0 => 1', () => {
-    expect(Util.factorial(0)).toBe(1);
-});
-test('Test factoriel de 1 => 1', () => {
-    expect(Util.factorial(1)).toBe(1);
-});
-test('Test factoriel de 2 => 2', () => {
-    expect(Util.factorial(2)).toBe(2);
-});
+    test('3 => 6', () => {
+        expect(Util.factorial(3)).toBe(6);
+    });
 
-test('Test factoriel de 3 => 6', () => {
-    expect(Util.factorial(3)).toBe(6);
-});
+    test('>= 3000 => Exception', () => {
+        expect(()=> {Util.factorial(3000)}).toThrow();
+    });
 
-test('Test factoriel de 3000', () => {
-    expect(()=> {Util.factorial(3000)}).toThrow();
+    test('Negative => Exception', () => {
+        expect(()=> {Util.factorial(-10)}).toThrow(/negative/);
+    });
 });
-
-test('Test factoriel -10', () => {
-    expect(()=> {Util.factorial(-10)}).toThrow(/negative/);
-});
-
 
 describe('isPrime', function () {
-
-    test('Test prime de 1 => false', () => {
+    test('1 => false', () => {
         expect(Util.isPrime(1)).toBe(false)
     });
-    test('Test prime de 0 => false', () => {
+    test('0 => false', () => {
         expect(Util.isPrime(0)).toBe(false)
     });
-    test('Test prime < 0 => throw exception', () => {
+    test('< 0 => Exception', () => {
         expect(() => { Util.isPrime(-10) }).toThrow('Unable to compute prime for n < 0');
     });
 
@@ -44,7 +43,7 @@ describe('isPrime', function () {
         [53, true],
         [55, false],
     ])(
-        'isPrime %i equals to %i',
+        '%i => %i',
         (n, expected) => {
             expect(Util.isPrime(n)).toBe(expected);
         }
@@ -52,28 +51,43 @@ describe('isPrime', function () {
 });
 
 describe('sumPrime', function() {
-    test('Test sumPrime de 1 => 0', () => {
+    test('1 => 0', () => {
         expect(Util.sumPrime(1)).toBe(0)
     });
-    test('Test sumPrime de 6 => 10', () => {
+    test('6 => 10', () => {
         expect(Util.sumPrime(6)).toBe(10)
     });
-    test('Test sumPrime de 8 => 17', () => {
+    test('8 => 17', () => {
         expect(Util.sumPrime(8)).toBe(17)
     });
-        test('Test sumPrime de 3 => 2', () => {
+        test('3 => 2', () => {
         expect(Util.sumPrime(3)).toBe(2)
     });
 });
 
 describe('fizzBuzz', function () {
-    test('Test fizzBuzz(15) to be [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]', () => {
+    test('15 => [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]', () => {
         expect(Util.fizzBuzz(15)).toStrictEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]);
     });
-    test('Test fizzBuzz(1) to be [1]', () => {
-        expect(Util.fizzBuzz(1)).toStrictEqual(["FizzBuzz",1]);
+    test('1 => [1]', () => {
+        expect(Util.fizzBuzz(1)).toStrictEqual([1]);
     });
-    test('Test fizzBuzz(0) to be []', () => {
-        expect(Util.fizzBuzz(0)).toStrictEqual(["FizzBuzz"]);
+    test('0 => []', () => {
+        expect(Util.fizzBuzz(0)).toStrictEqual([]);
+    });
+    test('-1 => []', () => {
+        expect(Util.fizzBuzz(-1)).toStrictEqual([]);
+    });
+});
+
+describe('cipher', function () {
+    test('"ABC" => "BCD"', () => {
+        expect(Util.cipher("ABC")).toBe("BCD");
+    });
+    test('"" => ""', () => {
+        expect(Util.cipher("")).toBe("");
+    });
+    test('"." => "/"', () => {
+        expect(Util.cipher(".")).toBe("/");
     });
 });
