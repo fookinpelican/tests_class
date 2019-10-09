7d451c88fb188e0f4425772c23f19e035bb1f885
fookinpelican <37412517+fookinpelican@users.noreply.github.com>
2019-10-09 23:03:03 +0200

    Add exclusion to Interval class

--- src/interval.js
+++ src/interval.js
@@ -1,9 +1,7 @@
 class Interval {
-    constructor(start, end, ...args) {
+    constructor(start, end) {
         if (start === undefined || end === undefined) {
             throw 'ERROR: Please provide "start" and "end" arguments';
-        } else if (args.length !== 0) {
-            throw 'ERROR: Too many arguments';
         } else if (typeof start !== 'number' || typeof end !== 'number') {
             throw 'ERROR: The type of "start" or "end" is incorrect';
         }
@@ -13,6 +11,7 @@ class Interval {
             start = end;
             end = tmp;
         }
+
         this.start = start;
         this.end = end;
     }
@@ -23,13 +22,13 @@ class Interval {
 
     /**
      * Exemple 1 :
-     *      interval1 = Interval(0, 4)
-     *      interval2 = Interval(2, 6)
+     *      interval1 = new Interval(0, 4)
+     *      interval2 = new Interval(2, 6)
      *      interval1.overlaps(interval2) => true
      *
      * Exemple 2 :
-     *      interval1 = Interval(2, 6)
-     *      interval2 = Interval(6, 10)
+     *      interval1 = new Interval(2, 6)
+     *      interval2 = new Interval(6, 10)
      *      interval1.overlaps(interval2) => false
      *
      * @param {Interval} interval
@@ -51,13 +50,13 @@ class Interval {
      * Retourne true si cet interval contient le paramètre interval
      *
      * Exemple 1 :
-     *      interval1 = Interval(0, 4)
-     *      interval2 = Interval(2, 4)
+     *      interval1 = new Interval(0, 4)
+     *      interval2 = new Interval(2, 4)
      *      interval1.includes(interval2) => true
      *
      * Exemple 2 :
-     *      interval1 = Interval(0, 4)
-     *      interval2 = Interval(2, 16)
+     *      interval1 = new Interval(0, 4)
+     *      interval2 = new Interval(2, 16)
      *      interval1.includes(interval2) => false
      *
      * @param {Interval} interval
@@ -75,13 +74,13 @@ class Interval {
      * Retourne l'union de deux intervals
      *
      * Exemple 1 :
-     *      interval1 = Interval(0, 4)
-     *      interval2 = Interval(2, 4)
+     *      interval1 = new Interval(0, 4)
+     *      interval2 = new Interval(2, 4)
      *      interval1.union(interval2) => [Interval(0, 4)]
      *
      * Exemple 2 :
-     *      interval1 = Interval(0, 4)
-     *      interval2 = Interval(6, 10)
+     *      interval1 = new Interval(0, 4)
+     *      interval2 = new Interval(6, 10)
      *      interval1.union(interval2) => [Interval(0, 4), Interval(6, 10)]
      *
      * @param {Interval} interval
@@ -103,7 +102,7 @@ class Interval {
 
             if (array[0].end === array[1].start) {
                 array[0].end = array[1].end;
-                array.pop()
+                array.pop();
             }
         } else {
             const start = this.start <= interval.start ? this.start : interval.start;
@@ -118,13 +117,13 @@ class Interval {
      * Retourne l'intersection de deux intervals
      *
      * Exemple 1 :
-     *      interval1 = Interval(0, 4)
+     *      interval1 = new Interval(0, 4)
      *      interval2 = Interval(2, 4)
      *      interval1.intersection(interval2) => Interval(2, 4)
      *
      * Exemple 2 :
-     *      interval1 = Interval(0, 4)
-     *      interval2 = Interval(6, 10)
+     *      interval1 = new Interval(0, 4)
+     *      interval2 = new Interval(6, 10)
      *      interval1.intersection(interval2) => null
      *
      * @param {Interval} interval
@@ -147,20 +146,49 @@ class Interval {
      * Retourne l'exclusion de deux intervals
      *
      * Exemple 1 :
-     *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval2 =                                              ▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval1.exclusion(interval2) =>    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒     ▒▒▒▒▒▒▒▒
+     *      interval1 = new Interval(0, 4)
+     *      interval2 = new Interval(2, 4)
+     *      interval1.exclusion(interval2) => [Interval(0, 2)]
      *
      * Exemple 2 :
-     *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval2 =                                                      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval1.exclusion(interval2) =>    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+     *      interval1 = new Interval(0, 4)
+     *      interval2 = new Interval(6, 10)
+     *      interval1.exclusion(interval2) => [Interval(0, 4), Interval(6, 10)]
      *
      * @param {Interval} interval
      * @returns {Interval[]}
      */
     exclusion(interval) {
+        if (interval.constructor.name !== 'Interval') {
+            throw 'ERROR: the parameter must be an Interval'
+        } else if (! this.overlaps(interval) && this.end < interval.start) {
+            return [
+                new Interval(this.start, this.end),
+                new Interval(interval.start, interval.end),
+            ];
+        } else if (! this.overlaps(interval) && this.start > interval.end) {
+            return [
+                new Interval(interval.start, interval.end),
+                new Interval(this.start, this.end),
+            ];
+        } else if (this.start === interval.start && this.end === interval.end) {
+            return new Array();
+        } else {
+            const array = new Array();
+            if (this.start < interval.start) {
+                array.push(new Interval(this.start, interval.start));
+            } else if (this.start > interval.start) {
+                array.push(new Interval(interval.start, this.start));
+            }
 
+            if (this.end > interval.end) {
+                array.push(new Interval(this.end, interval.end));
+            } else if (this.end < interval.end) {
+                array.push(new Interval(interval.end, this.end));
+            }
+
+            return array;
+        }
     };
 }
 
