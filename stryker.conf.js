af3f77c9905a9d6dd7d493c46e4fbc7d878774bf
fookinpelican <37412517+fookinpelican@users.noreply.github.com>
2019-10-09 23:31:12 +0200

    Create stryker.conf.js

---
+++ stryker.conf.js
@@ -0,0 +1,18 @@
+module.exports = function(config) {
+  config.set({
+    mutator: "javascript",
+    packageManager: "npm",
+    reporters: ["html", "clear-text", "progress", "dashboard"],
+    testRunner: "jest",
+    transpilers: [],
+    coverageAnalysis: "off",
+    mutate: [
+        "**/interval.js"
+    ],
+    thresholds: {
+        high: 80,
+        low: 70,
+        break: 70,
+    }
+  });
+};
